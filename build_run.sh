c06267016bf6ba041624f1260ba1de4374282696
alejandrox1 <alarcj137@gmail.com>
2018-06-04 18:13:10 -0500

    build_run.sh cleaned up script

--- build_run.sh
+++ build_run.sh
@@ -4,10 +4,6 @@ set -o pipefail
 
 IMG="ubuntu.simg"
 
-# Uncomment it to rebuild the image.
-#rm -rf "${IMG}"
-
-make 
 if [ ! -f "${IMG}" ]; then
     sudo singularity build "${IMG}" Singularity 
 fi
