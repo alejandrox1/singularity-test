0a2d184b256ee37b205a5b620402e1db680f5a0e
alejandrox1 <alarcj137@gmail.com>
2018-06-04 22:55:34 -0500

    module/stampede2_singularity_install.sh reentrant installation - rm existing dirs with repeated src

--- module/stampede2_singularity_install.sh
+++ module/stampede2_singularity_install.sh
@@ -17,6 +17,7 @@ SINGULARITY_VERSION="2.5.1"
 
 mkdir -p "${LIBARCHIVE_SRC}" && \
     cd "${LIBARCHIVE_SRC}" && \
+    [ -d "${LIBARCHIVE_VERSION}" ] && rm -rf "${LIBARCHIVE_VERSION}" && \
     git clone https://github.com/libarchive/libarchive "${LIBARCHIVE_VERSION}" && \
     cd "${LIBARCHIVE_VERSION}" && \
     git fetch origin --tags --prune && \
@@ -29,6 +30,7 @@ mkdir -p "${LIBARCHIVE_SRC}" && \
 
 mkdir -p "${SINGULARITY_SRC}" && \
     cd "${SINGULARITY_SRC}" && \
+    [ -d "v${SINGULARITY_VERSION}" ] && rm -rf "v${SINGULARITY_VERSION}" && \
     git clone https://github.com/singularityware/singularity.git "v${SINGULARITY_VERSION}" && \
     cd "v${SINGULARITY_VERSION}" && \
     git fetch origin --tags --prune && \
