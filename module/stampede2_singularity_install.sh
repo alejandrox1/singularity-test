a73764cf3d8c804e2ec295523276683243ae74f9
alejandrox1 <alarcj137@gmail.com>
2018-06-04 22:50:25 -0500

    module/stampede2_singularity_install.shinstall libarchive-dev from src

--- module/stampede2_singularity_install.sh
+++ module/stampede2_singularity_install.sh
@@ -2,19 +2,38 @@
 set -e                                                                          
 set -o pipefail                                                                 
 
+module load gcc
+
 # Compiler optimizations.
-export CC=`which icc`
-export CFLAGS="-xMIC-AVX512"
-export LDFLAGS="-xMIC-AVX512"
+export CC=gcc
 
 # SRC destination.
-SINGULARITY_SRC="/work/05692/jochoa/stampede2/apps/singularity"
+APPS_SRC="/work/05692/jochoa/stampede2/apps"
+LIBARCHIVE_SRC="${APPS_SRC}/libarchive"
+LIBARCHIVE_VERSION="v3.3.2"
+SINGULARITY_SRC="${APPS_SRC}/singularity"
+SINGULARITY_VERSION="2.5.1"
+
+
+mkdir -p "${LIBARCHIVE_SRC}" && \
+    cd "${LIBARCHIVE_SRC}" && \
+    git clone https://github.com/libarchive/libarchive "${LIBARCHIVE_VERSION}" && \
+    cd "${LIBARCHIVE_VERSION}" && \
+    git fetch origin --tags --prune && \
+    git checkout "${LIBARCHIVE_VERSION}" && \
+    build/autogen.sh && \
+    ./configure --prefix="${LIBARCHIVE_VERSION}" && \
+    make && \
+    make install
+
 
 mkdir -p "${SINGULARITY_SRC}" && \
     cd "${SINGULARITY_SRC}" && \
-    git clone https://github.com/singularityware/singularity.git v2.5.1 && \
-    cd v2.5.1 && \
+    git clone https://github.com/singularityware/singularity.git "v${SINGULARITY_VERSION}" && \
+    cd "v${SINGULARITY_VERSION}" && \
+    git fetch origin --tags --prune && \
+    git checkout "${SINGULARITY_VERSION}" && \
     ./autogen.sh && \
-    ./configure --prefix="${SINGULARITY_SRC}/v2.5.1" && \ 
+    ./configure --prefix="${SINGULARITY_SRC}/v${SINGULARITY_VERSION}" && \
     make && \
     make install
